71b014e447735d6a35c9dd187b30ee64bcd4c52c
yokenzan <31175068+yokenzan@users.noreply.github.com>
2024-08-04 14:57:36 +0900

    impl hello_db

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/hello/', include('api.hello.urls')),
+    path('api/hello_db/', include('api.hello_db.urls')),
 ]
